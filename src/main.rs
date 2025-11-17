618cef27d9bc76e2a7c066090d80fde85261a041
Danton <danton_tomacheski@outlook.com>
2025-11-17 00:16:43 -0300

    feat: adicionar handlers HTTP com Axum e testes de integração
    - Adicionar rotas /hello (query params) e /hello2/:name (path params)
    - Implementar handlers com extratores Query e Path
    - Configurar servidor HTTP na porta 8080
    - Adicionar testes de integração com httpc-test
    - Incluir dependências: anyhow, httpc-test, serde

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,42 @@
+#[allow(unused)]
+
 use std::net::SocketAddr;
 
-use axum::{Router, response::Html, routing::get};
+use axum::{Router, extract::{Path, Query}, response::{Html, IntoResponse}, routing::get};
+use serde::Deserialize;
 
 #[tokio::main]
-async fn main() {
-    let hello_routes = Router::new().route(
-        "/hello-world",
-        get(|| async { Html("<h1>Hello Jovana</h1>") })
-    );
+async fn main(){
+    let hello_routes = Router::new()
+    .route("/hello",get(handler_hello))
+    .route("/hello2/{name}", get(handler_hello2));
 
-    let addr = SocketAddr::from(([127,0,0,1], 8080));
-
+    // #region --- Server init ---
+    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    println!("Listening on {addr}");
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
-
     axum::serve(listener, hello_routes).await.unwrap();
+    // #endregion --- Server init ---
 }
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>
+}
+
+// #region --- Handler Hello ---
+// e.g., `/hello?name=Danton`
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+
+    let name = params.name.as_deref().unwrap_or("World");
+    Html(format!("<h1>Hello {name}</h1>"))
+}
+// #endregion --- Handler Hello ---
+
+// #region --- Handler hello 2 ---
+// e.g., `/hello2/Danton/`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name}", "HANDLER_HELLO2");
+    Html(format!("<h2>Hello {name}</h2>"))
+}
+// #endregion --- Handler hello 2 ---
---
+++ tests/quick_dev.rs
@@ -0,0 +1,13 @@
+#[allow(unused)]
+
+use anyhow::Result;
+
+#[tokio::test]
+async fn quick_dev() -> Result<()> {
+
+    let hc = httpc_test::new_client("http://127.0.0.1:8080")?;
+
+    hc.do_get("/hello?name=Danton").await?.print().await?;
+    hc.do_get("/hello2/Jovana").await?.print().await?;
+    Ok(())
+}
